4ebcfb533f780e861eefaedc892b402102753881
Minsuh Jo <minsuh3203@gmail.com>
2022-03-31 12:31:53 +0900

    display organization/repository using viewModel

--- app/src/main/java/com/thingsflow/internapplication/ui/detail/IssueDetailFragment.kt
+++ app/src/main/java/com/thingsflow/internapplication/ui/detail/IssueDetailFragment.kt
@@ -1,7 +1,36 @@
 package com.thingsflow.internapplication.ui.detail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.thingsflow.internapplication.databinding.IssueDetailFragmentBinding
 
 class IssueDetailFragment : Fragment() {
 
+    companion object {
+        fun newInstance() = IssueDetailFragment()
+    }
+
+    private lateinit var viewModel: IssueDetailViewModel
+    private var _binding: IssueDetailFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = IssueDetailFragmentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
+
--- app/src/main/java/com/thingsflow/internapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/thingsflow/internapplication/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.thingsflow.internapplication.R
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.thingsflow.internapplication.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,12 +17,25 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var organizationTextView: TextView
+    private lateinit var repositoryTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -28,7 +43,21 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
 
+        viewModel.setOrganization("google")
+        viewModel.setRepository("dagger")
 
+        organizationTextView = binding.organizationName
+        repositoryTextView = binding.repositoryName
+
+        observe()
     }
 
+    private fun observe() = with(viewModel){
+        organization.observe(viewLifecycleOwner, Observer {
+            organizationTextView.text = it
+        })
+        repository.observe(viewLifecycleOwner, Observer {
+            repositoryTextView.text = it
+        })
+    }
 }
