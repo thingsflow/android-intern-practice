d325ac7dce4adde16adbec75f75e92ca1b2b03a2
Minsuh Jo <minsuh3203@gmail.com>
2022-03-31 09:14:37 +0900

    changed reviewed parts

--- app/src/main/java/com/thingsflow/internapplication/ui/main/MainViewModel.kt
+++ app/src/main/java/com/thingsflow/internapplication/ui/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.thingsflow.internapplication.ui.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.thingsflow.internapplication.data.IssueData
@@ -7,8 +8,13 @@ import com.thingsflow.internapplication.data.IssueData
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 
-    var issueList = MutableLiveData<ArrayList<IssueData>>()
-    var organization = MutableLiveData<String>()
-    var repository = MutableLiveData<String>()
+    private val _issueList = MutableLiveData<ArrayList<IssueData>>()
+    val issueList : LiveData<ArrayList<IssueData>> = _issueList
+
+    private val _organization = MutableLiveData<String>()
+    val organization : LiveData<String> = _organization
+
+    private val _repository = MutableLiveData<String>()
+    val repository : LiveData<String> = _repository
 
 }
