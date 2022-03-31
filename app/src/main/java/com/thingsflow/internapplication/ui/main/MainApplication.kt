d43401a025258cdac5013a8901464890b782cb80
dycha0430 <dycha0430@hanyang.ac.kr>
2022-03-31 10:54:41 +0900

    hilt config

---
+++ app/src/main/java/com/thingsflow/internapplication/ui/main/MainApplication.kt
@@ -0,0 +1,8 @@
+package com.thingsflow.internapplication.ui.main
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application() {
+}
