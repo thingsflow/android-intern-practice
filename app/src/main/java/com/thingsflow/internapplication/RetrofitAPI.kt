8e98e10cf7b03600a75fcb3ce1d838ff45db84bb
dycha0430 <dycha0430@hanyang.ac.kr>
2022-04-06 17:51:39 +0900

    Load github api: Rxjava -> Coroutine flow

--- app/src/main/java/com/thingsflow/internapplication/RetrofitAPI.kt
+++ app/src/main/java/com/thingsflow/internapplication/RetrofitAPI.kt
@@ -7,5 +7,8 @@ import retrofit2.http.Path
 
 interface RetrofitAPI {
     @GET("{org}/{repo}/issues")
-    fun getIssues(@Path("org") org: String, @Path("repo") repo: String) : Single<List<Item.Issue>>
+    fun getIssuesRx(@Path("org") org: String, @Path("repo") repo: String) : Single<List<Item.Issue>>
+
+    @GET("{org}/{repo}/issues")
+    suspend fun getIssuesFlow(@Path("org") org: String, @Path("repo") repo: String) : List<Item.Issue>
 }
--- app/src/main/java/com/thingsflow/internapplication/ui/main/MainRepository.kt
+++ app/src/main/java/com/thingsflow/internapplication/ui/main/MainRepository.kt
@@ -3,12 +3,21 @@ package com.thingsflow.internapplication.ui.main
 import com.thingsflow.internapplication.RetrofitAPI
 import com.thingsflow.internapplication.data.Item
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
     private val retrofitAPI: RetrofitAPI
 ) {
-    fun getIssues(org: String, repo: String) : Single<List<Item.Issue>> {
-        return retrofitAPI.getIssues(org, repo)
+    fun getIssuesRx(org: String, repo: String) : Single<List<Item.Issue>> {
+        return retrofitAPI.getIssuesRx(org, repo)
+    }
+
+    fun getIssuesFlow(org: String, repo: String) : Flow<List<Item.Issue>> {
+        return flow {
+            val issues = retrofitAPI.getIssuesFlow(org, repo)
+            emit(issues)
+        }
     }
 }
