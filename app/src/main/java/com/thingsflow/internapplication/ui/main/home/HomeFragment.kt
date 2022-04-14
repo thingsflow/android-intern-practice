a6fc578990c68b5efd7062b369771be5e6e93261
dycha0430 <dycha0430@hanyang.ac.kr>
2022-04-14 11:56:59 +0900

    장르 별 작품 섹션 list view

--- app/src/main/java/com/thingsflow/internapplication/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/thingsflow/internapplication/ui/main/home/HomeFragment.kt
@@ -1,10 +1,18 @@
 package com.thingsflow.internapplication.ui.main.home
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.thingsflow.internapplication.base.architecture.aac.observe
+import com.thingsflow.internapplication.base.architecture.base.dpToPx
 import com.thingsflow.internapplication.base.architecture.base.viewbinding.BaseFragment
+import com.thingsflow.internapplication.base.ui.list.adapter.AutoBindHolderFactory
+import com.thingsflow.internapplication.base.ui.list.adapter.IntervalItemDecoration
+import com.thingsflow.internapplication.base.ui.list.adapter.buildAdapter
 import com.thingsflow.internapplication.databinding.MainFragmentBinding
+import com.thingsflow.internapplication.model.OnStageNovelCover
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,16 +24,48 @@ class HomeFragment : BaseFragment<HomeViewModel, MainFragmentBinding>() {
 
     override val viewModel by viewModels<HomeViewModel>()
 
-    override fun loadData() {
-
+    private val event by lazy {
+        HomeEvent(viewModel)
     }
 
-    override fun setupUi() {
+    private val novelCoverAdapter by lazy {
+        AutoBindHolderFactory<OnStageNovelCover>()
+            .add(
+                OnStageNovelCover::class,
+                NovelCoverHolder.DIFF,
+                event,
+                NovelCoverHolder.CREATOR
+            )
+            .buildAdapter()
+    }
 
+    override fun loadData() {
+        viewModel.load()
+    }
+
+    override fun setupUi() = with(binding) {
+        recyclerByGenre.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        recyclerByGenre.adapter = novelCoverAdapter
+        val padding = 15f.dpToPx(requireContext())
+        recyclerByGenre.addItemDecoration(
+            IntervalItemDecoration(
+                startOffset = padding,
+                endOffset = padding,
+                offset = padding
+            )
+        )
+    }
+
+    private fun renderNovelCoverByGenreList(list: List<OnStageNovelCover>) {
+        novelCoverAdapter.submitList(list)
     }
 
     override fun observeUi() {
-
+        with(viewModel) {
+            observe(onStageNovelCoversByGenre) {
+                renderNovelCoverByGenreList(it)
+            }
+        }
     }
 
     override fun createBinding(
