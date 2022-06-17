65be4dc0e95bf415c8ba30d263ff332b398d8f38
hyunjeong <65072995+hyunjeong408@users.noreply.github.com>
2022-06-17 11:53:23 +0900

    Feature/hyunjeong mvvm (#29)
    * feat: Add mvvm feature
    
    * fix: Fix mvvm structure
    
    mvvm, livedata, observe
    
    * feat: Add ViewBinding
    
    * fix: observe null check
    
    * fix: observe null check

--- app/src/main/java/com/thingsflow/internapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/thingsflow/internapplication/ui/main/MainFragment.kt
@@ -2,11 +2,16 @@ package com.thingsflow.internapplication.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.thingsflow.internapplication.R
+import android.widget.Button
+import androidx.viewbinding.ViewBinding
+import com.thingsflow.internapplication.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -14,19 +19,43 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+        val btn: Button = binding.button
+        viewModel.vmText.observe(viewLifecycleOwner, Observer {
+            binding.textview.text = it
+            if(it.isNotBlank()){
+                binding.textview.setVisibility(View.VISIBLE)
+                binding.button.setVisibility(View.GONE)
+            }
+            else {
+                binding.textview.setVisibility(View.GONE)
+                binding.button.setVisibility(View.VISIBLE)
+            }
+        })
+        btn.setOnClickListener {
+            viewModel.initText()
+        }
     }
 
 }
--- app/src/main/java/com/thingsflow/internapplication/ui/main/MainViewModel.kt
+++ app/src/main/java/com/thingsflow/internapplication/ui/main/MainViewModel.kt
@@ -1,7 +1,15 @@
 package com.thingsflow.internapplication.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    protected val _vmText = MutableLiveData<String>()
+    val vmText: LiveData<String> = _vmText
+
+    fun initText(){
+        _vmText.value = "Hello, World!"
+    }
 }
